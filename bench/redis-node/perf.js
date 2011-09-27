cb2b12058cd8aef8fbf198464beb7907691457aa
Tim Smart <tim@fostle.com>
2011-09-27 16:30:52 +1300

    Remove un-neccesary bench stuff.

--- bench/redis-node/perf.js
+++
@@ -1,9 +0,0 @@
-exports.toSmallString = function (buffer, until) {
-    until = until || buffer.length;
-    var ret = "",
-        i = 0;
-    for ( ; i < until; i++) {
-        ret += String.fromCharCode(buffer[i]);
-    }
-    return ret;
-};
